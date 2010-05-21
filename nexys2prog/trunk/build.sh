dc3537fa03e8502e8c613488b0f5392119f549b8
haukex <haukex@16d96410-3d24-0410-b1d3-9a6b7672f313>
2010-05-21 09:40:46 +0000

    minor updates to nexys2prog build.sh
    git-svn-id: svn://svn.code.sf.net/p/ixo-jtag/code@185 16d96410-3d24-0410-b1d3-9a6b7672f313

--- nexys2prog/trunk/build.sh
+++ nexys2prog/trunk/build.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # Minimal Xilinx build environment to show off the Nexys 2 programming
 # script.
@@ -9,10 +9,16 @@ PROJ=led
 #PART=xc3s500e-fg320-5
 PART=xc3s1200e-fg320-5
 
+# You may need to comment in the following if the Xilinx variables are not
+# yet set in your environment
+#source /opt/Xilinx/11.1/settings32.sh
+
 echo run -ifn $PROJ.v -ifmt verilog -ofn $PROJ.ngc -p $PART -top $PROJ | xst
 ngdbuild -i -p $PART $PROJ.ngc
 map -p $PART $PROJ.ngd
 par $PROJ.ncd ${PROJ}_par.ncd $PROJ.pcf
 bitgen ${PROJ}_par.ncd $PROJ.bit $PROJ.pcf
 
-./nexys2prog $PROJ.bit
+# You may remove the -v (--verbose) switch for less verbose output.
+./nexys2prog -v $PROJ.bit
+
